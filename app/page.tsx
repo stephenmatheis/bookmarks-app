32b54d7717ff6a680ba545a3c02ee9942cf90d6c
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-03 12:27:01 -0400

    feat: home page (WIP)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import styles from './page.module.scss';
+import { createClient } from '@/lib/supabase/server';
 
-export default function Home() {
+export default async function Home() {
+    const startIndex = 0;
+    const endIndex = 10;
+    const supabase = await createClient();
+    const { data } = await supabase.from('bookmarks').select().lte('id', endIndex);
+
     return (
-        <main>
-            <div>Hello world!</div>
-            <Link href="/protected">Protected route</Link>
+        <main className={styles.main}>
+            <h1>Bookmarks</h1>
+            <div className={styles.bookmarks}>
+                {data?.slice(startIndex, endIndex).map((bookmark, index) => {
+                    return (
+                        <div key={index} className={styles.bookmark}>
+                            <h2>{bookmark.title}</h2>
+                            <Link href={bookmark.url} target="_blank" rel="noopener noreferrer">
+                                {bookmark.url}
+                            </Link>
+                            <div className={styles.image}>
+                                <Image
+                                    src={`/screenshots/${index}-desktop.png`}
+                                    alt={bookmark.title}
+                                    width={800}
+                                    height={800}
+                                />
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </main>
     );
 }
