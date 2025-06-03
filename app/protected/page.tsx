e9a5b3718189fef10e72fb69b531ee5820787b1d
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-03 12:26:56 -0400

    Update page.tsx

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -3,8 +3,8 @@ import { createClient } from '@/lib/supabase/server';
 
 export default async function ProtectedPage() {
     const supabase = await createClient();
-
     const { data, error } = await supabase.auth.getUser();
+
     if (error || !data?.user) {
         redirect('/auth/login');
     }
