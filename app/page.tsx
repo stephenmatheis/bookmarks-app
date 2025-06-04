efbe11a090a57a831af206ecc0c31224eb6add36
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-03 22:56:23 -0400

    feat: supabase images

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,46 @@ export default async function Home() {
                 {data?.slice(startIndex, endIndex).map((bookmark, index) => {
                     return (
                         <div key={index} className={styles.bookmark}>
-                            <h2>{bookmark.title}</h2>
-                            <Link href={bookmark.url} target="_blank" rel="noopener noreferrer">
-                                {bookmark.url}
-                            </Link>
-                            <div className={styles.image}>
+                            <div className={styles.info}>
+                                <div className={styles.avatar}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        fill="currentColor"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path d="M3 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2zm6 11a1 1 0 1 0-2 0 1 1 0 0 0 2 0" />
+                                    </svg>
+                                </div>
+                                <div className={styles.source}>iPhone</div>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
+                                </svg>
+                                <div className={styles.date}>{new Date(bookmark.created_at).toLocaleString()}</div>
+                            </div>
+                            {bookmark.image_url ? (
                                 <Image
-                                    src={`/screenshots/${index}-desktop.png`}
+                                    src={bookmark.image_url}
                                     alt={bookmark.title}
-                                    width={800}
-                                    height={800}
+                                    width={1920}
+                                    height={1080}
+                                    priority
                                 />
+                            ) : (
+                                <div className={styles.placeholder}>Image missing</div>
+                            )}
+                            <div className={styles.text}>
+                                <div className={styles.title}>{bookmark.title}</div>
+                                <Link href={bookmark.url} target="_blank" rel="noopener noreferrer">
+                                    {bookmark.url}
+                                </Link>
                             </div>
                         </div>
                     );
